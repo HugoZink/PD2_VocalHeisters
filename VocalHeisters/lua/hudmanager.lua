7824f5776ed5ba9cdd96cb6b855ca2ddc5a24976
Hugo Zink <hugo.zink@hotmail.com>
2020-09-20 19:37:38 +0200

    Fixed anticipation lines not working correctly

--- VocalHeisters/lua/hudmanager.lua
+++ VocalHeisters/lua/hudmanager.lua
@@ -1,9 +1,12 @@
 dofile(ModPath .. "vocalheisterscore.lua")
 
 local anticipation_lines = {
-    [3] = "g70",
-    [2] = nil,
-    [1] = "g50"
+    [2] = "g70",
+    [3] = nil,
+    [4] = "g50",
+    [6] = "g70",
+    [7] = nil,
+    [8] = "g50"
 }
 
 -- Say a line when assaults are anticipating
@@ -12,6 +15,7 @@ local anticipation_lines = {
 -- I think without one of those mods, this part doesn't work at all
 if VocalHeisters.Settings.enable_anticipation_lines then
     Hooks:PostHook(HUDManager, "sync_assault_dialog", "vocalheisters_sync_assault_dialog_playassaultlines", function(self, index)
+
         if not anticipation_lines[index] then
             return
         end
